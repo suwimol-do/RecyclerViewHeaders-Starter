684d6443fd3163324823a717190d6ea6c55df334
Suwimol <Suwimol.do@rmuti.ac.th>
2020-09-05 17:38:37 +0700

    7.5.6 Task: Extend the header to span across the screen

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -133,7 +133,12 @@ class SleepTrackerFragment : Fragment() {
         })
 
         val manager = GridLayoutManager(activity, 3)
-        binding.sleepList.layoutManager = manager
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) =  when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
 
         return binding.root
     }
